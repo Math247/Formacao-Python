601384b427f596de348c52ca1155b5632e95ef2b
Matheus Carregal <matheuschabib@gmail.com>
2023-03-21 18:05:04 -0300

    Create conta.py

---
+++ Python Entendendo a Orientação a Objetos/conta.py
@@ -0,0 +1,17 @@
+class Conta:
+
+
+    def __init__(self, numero, titular, saldo, limite):
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+
+    def extrato(self):
+        print("Saldo {}".format(self.saldo))
+    
+    def deposita(self, valor):
+        self.saldo += valor
+    
+    def saca(self, valor):
+        self.saldo -= valor
